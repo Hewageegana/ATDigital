204c26ef34ea4b0f7f2385cde7cdb11db5f12deb
hewageegana <rusiru2000hewageegana@gmail.com>
2023-12-20 00:10:56 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from "../src/components/main"
-import Footer from "../src/components/footer"
-import Nav from "../src/components/navbar"
+
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Nav from '../src/components/navigationBar'
 import Footer from '../src/components/footer'
 import Navbar from './components/navbar';
 
