f46f0c09a05177b0e777b79c574e2ee63a77aeae
hewageegana <rusiru2000hewageegana@gmail.com>
2023-12-20 00:17:24 +0530

    href

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark" style={{ backgroundColor: "#6B3CC9" }}>
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">
+                <a class="navbar-brand" href="/">
                     <img src={logo} alt="" className='logo' />
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,16 +15,16 @@ export default function Navbar() {
                 <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">SERVICE</a>
+                            <a class="nav-link" aria-current="page" href="/">SERVICE</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">ABOUT US</a>
+                            <a class="nav-link" href="/">ABOUT US</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">CONTACT US</a>
+                            <a class="nav-link" href="/">CONTACT US</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">CAREERS</a>
+                            <a class="nav-link" href="/">CAREERS</a>
                         </li>
                        
                     </ul>
